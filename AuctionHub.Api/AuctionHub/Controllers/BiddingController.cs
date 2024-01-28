fb213ae90d35182837893cba4f140d06a18dc97f
Kingkaspev <kingsleychiboy22@gmail.com>
2024-01-28 06:45:19 +0100

    notification and bidding controllers

---
+++ AuctionHub.Api/AuctionHub/Controllers/BiddingController.cs
@@ -0,0 +1,44 @@
+﻿using AuctionHub.Application.Interfaces.Services;
+using AuctionHub.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHub.Controllers
+{
+    [ApiController]
+    [Route("api/bidding")]
+    public class BiddingController : ControllerBase
+    {
+        private readonly IBiddingService _biddingService;
+
+        public BiddingController(IBiddingService biddingService)
+        {
+            _biddingService = biddingService;
+        }
+
+        [HttpPost("start-auction")]
+        public async Task<IActionResult> StartAuctionAsync([FromBody] BiddingRoom biddingRoom)
+        {
+            var response = await _biddingService.StartAuctionAsync(biddingRoom);
+
+            if (response.Succeeded)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response);
+        }
+
+        [HttpPost("submit-bid")]
+        public async Task<IActionResult> SubmitBidAsync([FromBody] Bid bid)
+        {
+            var response = await _biddingService.SubmitBidAsync(bid);
+
+            if (response.Succeeded)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response);
+        }
+    }
+}
---
+++ AuctionHub.Api/AuctionHub/Controllers/NotificationController.cs
@@ -0,0 +1,44 @@
+﻿using AuctionHub.Application.Interfaces.Services;
+using AuctionHub.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHub.Controllers
+{
+    [ApiController]
+    [Route("api/notification")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpPost("notify-participants")]
+        public async Task<IActionResult> NotifyParticipantsAsync([FromBody] Bid bid)
+        {
+            var response = await _notificationService.NotifyParticipantsAsync(bid);
+
+            if (response.Succeeded)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response);
+        }
+
+        [HttpPost("notify-auction-conclusion")]
+        public async Task<IActionResult> NotifyAuctionConclusionAsync([FromBody] BiddingRoom biddingRoom)
+        {
+            var response = await _notificationService.NotifyAuctionConclusionAsync(biddingRoom);
+
+            if (response.Succeeded)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response);
+        }
+    }
+}
